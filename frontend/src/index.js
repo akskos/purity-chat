9d867fda1df068d8d5ef50a909279fc4098a2385
Akseli Koskinen <koskinen.akseli@gmail.com>
2019-05-17 21:44:48 +0300

    test click event

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
-const Index = () => {
-  return <div>Hello React!</div>;
-};
+import style from "./index.css";
+
+const dostuff = () => {
+  console.log("hello");
+}
+
+const Index = () => (
+  <input type="button" onClick={dostuff} value="click" />
+);
 
 ReactDOM.render(<Index />, document.getElementById("index"));
